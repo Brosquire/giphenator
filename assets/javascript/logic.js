dad171e8a43af4617ce87c795dadb512cb2c8e1c
Zachary Ferreira <Pearlybrah@github.com>
2019-07-15 20:48:19 -0700

    starting logic for api call

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,15 +1,36 @@
 $(document).ready(() => {
-  const API_URL = process.env.API_URL;
-  const API_KEY = process.env.KEY;
   //Giph initial loader function
-  (() => {
-    let queryURL = API_URL + "search?q=greenbaypackers" + API_KEY + "&limit=10";
+  const initialGiphGetter = () => {
+    let basequeryURL =
+      "https://api.giphy.com/v1/gifs/search?q=sports+greenbaypackers&api_key=9vTuLskEWKQPmQQvX6cL6ynEuxnBsLEb&limit=10";
+    apiCall(basequeryURL);
+  };
 
+  //ajax api function
+  const apiCall = params => {
     $.ajax({
-      url: queryURL,
+      url: params,
       method: "GET"
     }).then(response => {
       console.log(response);
     });
-  })();
+  };
+
+  //user search api giph function
+  const userGiphGetter = () => {
+    let userInput = $("#userInput");
+    let userURL =
+      "https://api.giphy.com/v1/gifs/search?q=sports+" +
+      userInput +
+      "&api_key=9vTuLskEWKQPmQQvX6cL6ynEuxnBsLEb&limit=10";
+
+    apiCall(userURL);
+  };
+
+  initialGiphGetter();
+
+  //on submit of user input function
+  $(document).on("#submit", () => {
+    userGiphGetter().then(console.log(data));
+  });
 });
